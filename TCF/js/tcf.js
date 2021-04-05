7fc9a7f5ef6f4d271e5a079edc964a6427ea0e07
Joel Campos <joelcamposoliva9000@gmail.com>
2021-04-05 15:27:21 +0200

    First release

--- TCF/js/tcf.js
+++ TCF/js/tcf.js
@@ -1,4 +1,5 @@
 /* JS FOR TCF COMPONENTS */
+console.log("Cargado tcf.js (creado por Joel Campos)");
 
 let fields = {};
 
@@ -28,4 +29,16 @@ function NavToggle(id) {
     const navList = document.getElementById(id);
     
 }
+
+function getContrastYIQ(hexcolor){ // Esta función es copiada de Internet :(
+    hexcolor = hexcolor.replace("#", "");
+    var r = parseInt(hexcolor.substr(0,2),16);
+    var g = parseInt(hexcolor.substr(2,2),16);
+    var b = parseInt(hexcolor.substr(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? 'black' : 'white';
+} 
+
 document.documentElement.style.setProperty('--color-primary', sessionStorage.getItem("theme")); // Aplicar tema
+document.documentElement.style.setProperty('--color-contrast-primary', getContrastYIQ(sessionStorage.getItem("theme")));
+//document.body.style.backgroundColor = getContrastYIQ(sessionStorage.getItem("theme"));
